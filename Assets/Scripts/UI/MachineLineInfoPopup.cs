c8538e1fb3f10502fdc956927105bbf1d81c7755
Christopher Sauzon <csauzon0@gmail.com>
2026-06-25 11:50:24 +0200

    Translation of InfoMachineLine

--- Assets/Scripts/UI/MachineLineInfoPopup.cs
+++ Assets/Scripts/UI/MachineLineInfoPopup.cs
@@ -59,9 +59,9 @@ public class MachineLineInfoPopup : MonoBehaviour
     {
         _infoText.text =
             $"<b>Machines</b>  {_line.MachineCount}\n" +
-            $"<b>Niveau</b>  {_line.UpgradeLevel}\n" +
-            $"<b>NPCs actifs</b>  {_line.TotalNPCsOnLine}\n" +
-            $"<b>Revenus totaux</b>  {_line.TotalRevenue:F0}$\n" +
-            $"<b>Revenu/min</b>  {_line.TotalRevenuePerMinute:F1}$/min";
+            $"<b>Level</b>  {_line.UpgradeLevel}\n" +
+            $"<b>Active NPCs</b>  {_line.TotalNPCsOnLine}\n" +
+            $"<b>Total Revenue</b>  {_line.TotalRevenue:F0}$\n" +
+            $"<b>Revenue/min</b>  {_line.TotalRevenuePerMinute:F1}$/min";
     }
 }
